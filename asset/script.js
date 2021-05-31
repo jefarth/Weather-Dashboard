c190829941af4eebe9b9b44d4f56a21b750e62fc
Jeremiah Farthing <jeremiah.farthing@gmail.com>
2021-05-30 22:44:18 -0600

    Created HTML framework

--- asset/script.js
+++ asset/script.js
@@ -1,6 +1,6 @@
-var APIKey = `4cacb3ab041418253b8abd5130a34093`;
-var city;
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+// var APIKey = `&appid=4cacb3ab041418253b8abd5130a34093`;
+// var city;
+// var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + APIKey;
 
-fetch(queryURL);
-console.log(queryURL);
+// fetch(queryURL);
+// console.log(queryURL);
